17cb69ff3b58d60abdcddac9ddcd5335a936e25f
mattenbar <matthw.enbar@gmail.com>
2021-05-20 14:05:11 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Reviews from './components/reviews'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  //uToken= 'S1lfzSrxmSi2pBUqXERPJLV6mKtxVm0yWV3q2JrY'
+ 
+
+  render(){
+    return(
+      <div className="App">
+        <Reviews />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/reviews.js
@@ -0,0 +1,55 @@
+import React from 'react'
+
+
+class Reviews extends React.Component{
+  
+  state = {
+    reviews: {}
+  }
+  
+  componentDidMount() {
+    fetch('https://api.yotpo.com/v1/apps/yqGNRSNYfvNyAt0cqAnknRn8ZY8CzmZjbpDdvXlB/reviews?utoken=S1lfzSrxmSi2pBUqXERPJLV6mKtxVm0yWV3q2JrY'
+    )
+      .then(response => response.json())
+      .then(data => 
+        this.setState(
+          {reviews: data.reviews}
+        )
+    )
+  }
+
+  render(){
+    console.log(this.state.reviews[0])
+    if (this.state.reviews.length > 0 ){
+      let reviewDiv = this.state.reviews.map((r) =>
+      {return(
+        <div className='review' key={r.id}>
+          <h3>Title: {r.title}</h3>
+          <h4>id: {r.id}</h4>
+          <h4>Content: {r.content}</h4>
+          <h4>rating: {r.score}</h4>
+          test
+        </div>
+        )
+      }
+    )
+    
+    
+      return(
+        <div className="reviewsContainer">
+          {reviewDiv}
+        </div>
+      )
+    }else{
+      return(
+        <div className="reviewsContainer">
+          
+
+        </div>
+      )
+
+    }
+}
+}
+
+export default Reviews
